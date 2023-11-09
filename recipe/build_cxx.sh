b295b30d5549bc48bda9eeaa293c92d752d00807
Silvio Traversaro <silvio@traversaro.it>
2023-11-09 12:06:24 +0100

    Enable PCL in build scripts

--- recipe/build_cxx.sh
+++ recipe/build_cxx.sh
@@ -28,6 +28,7 @@ cmake ${CMAKE_ARGS} -GNinja .. \
     -DFRAMEWORK_USE_LieGroupControllers:BOOL=ON \
     -DFRAMEWORK_USE_UnicyclePlanner:BOOL=ON \
     -DFRAMEWORK_USE_tomlplusplus:BOOL=ON \
+    -DFRAMEWORK_USE_PCL:BOOL=ON \
     -DFRAMEWORK_COMPILE_PYTHON_BINDINGS:BOOL=OFF
 
 cat CMakeCache.txt
